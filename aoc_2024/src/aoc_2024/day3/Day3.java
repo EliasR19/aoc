86f8b649e3369a0b1d3964238bca3d6527f35d73
EliasR19 <ereynoso2040@gmail.com>
2024-12-04 16:29:08 -0300

    part two try copy

--- aoc_2024/src/aoc_2024/day3/Day3.java
+++ aoc_2024/src/aoc_2024/day3/Day3.java
@@ -15,11 +15,11 @@ public class Day3 {
 		
 		
 		String input = Files.readString(Path.of("inputday3.txt"));
-		String inputTest = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+		String inputTest = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 		
 		String inputCurr = input;
 		
-		Pattern patt = Pattern.compile("mul\\(\\d{1,3}\\,\\d{1,3}\\)");
+		Pattern patt = Pattern.compile("mul\\((\\d{1,3})\\,(\\d{1,3})\\)");
 		Pattern doMul = Pattern.compile("do\\(\\)\\p{Graph}*?" + patt);
 		Pattern dontMul = Pattern.compile("don't\\(\\)\\p{Graph}*?" + patt);
 		
@@ -34,68 +34,39 @@ public class Day3 {
 		String str = "";
 		long count = 0;
 		
-		while(estado.find()) {
-
-			str = inputCurr.substring(estado.start(), estado.end());
-			System.out.println("str: " + str + " |Start: " + estado.start() + " |End: " + estado.end());
-			
-			int start = str.length() - 8;
-			
-			String post = str.substring(start, str.length());
-
-			
-			String numsStr = str.replace("mul(", "").replace(")", "");
-
-			String[] numsInt = numsStr.split(",");
-			int num1 = Integer.parseInt(numsInt[0]);
-			int num2 = Integer.parseInt(numsInt[1]);
-			
-			//System.out.println("a: " + num1 + " |b: " + num2);
-			
-			count = count + (num1 * num2);
-		}
+		System.out.println("Part 1" + partOne(inputCurr));
+		System.out.println("Part 2: " + partTwo(inputCurr));
 		
-		System.out.println("AlL: " + count);
-		System.out.println("dont: " + dont(inputCurr));
-		
-		
-		count = count - dont(inputCurr);
-		System.out.println(count);
 		
 	}
 	
-	public static long dont(String input) {
+	public static long partOne(String input) {
+		Pattern patt = Pattern.compile("mul\\((\\d{1,3})\\,(\\d{1,3})\\)");
+		Matcher matMul = patt.matcher(input);
 		long count = 0;
-		Pattern patt = Pattern.compile("mul\\(\\d{1,3}\\,\\d{1,3}\\)");
-		Pattern dontMul = Pattern.compile("don't\\(\\)\\p{Graph}*?" + patt);
-		Matcher matDont = dontMul.matcher(input);
-		
-		while(matDont.find()) {
+		while(matMul.find()) {
 
-			String str = input.substring(matDont.start(), matDont.end());
-			//System.out.println("str: " + str + " |Start: " + matDont.start() + " |End: " + matDont.end());
 			
+			long a = Long.parseLong(matMul.group(1));
+			long b = Long.parseLong(matMul.group(2));
+		//	System.out.println("MUL: " + a + " * " + b);
 			
-			String post = str.replaceAll("don't\\(\\)\\p{Graph}*?", "");
-			
-			//System.out.println("post: " + post);
-			
-			String numsStr = post.replaceAll("\\p{Graph}*mul\\(", "").replace(")", "");
-//			
-			//System.out.println("strNums: " + numsStr);
-//
-			String[] numsInt = numsStr.split(",");
-
-			int num1 = Integer.parseInt(numsInt[0]);
-			int num2 = Integer.parseInt(numsInt[1]);
-//			
-			//System.out.println("a: " + num1 + " |b: " + num2);
-//			
-			count = count + (num1 * num2);
-			
-			
+			count = count + (a * b);
 		}
 		return count;
+	}
+	
+	public static long partTwo(String input) {
+
+		Pattern dontMul = Pattern.compile("don't\\(\\).*?do\\(\\)");
+		
+		for(String s :dontMul.split(input) ) {
+			System.out.println(s);
+		}
+		
+		return partOne(String.join("", dontMul.split(input)));
+
+
 	}
 
 }
